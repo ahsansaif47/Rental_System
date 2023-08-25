96a0814cc223bf44119433f51b6bee566c80f69a
ahsansaif47 <ahsan_saif47@hotmail.com>
2023-08-26 00:25:20 +0500

    settings package added

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"Rental_System/Auth"
+	settings "./Settings"
 	"fmt"
 	"net/http"
 
@@ -12,6 +13,7 @@ func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/register_user", Auth.Register_user).Methods("POST")
 	router.HandleFunc("/login", Auth.Login).Methods("GET")
+	router.HandleFunc("/delete_user", settings.deleteUser).Methods("GET")
 
 	fmt.Println("Server is running on http://localhost:8080")
 	http.ListenAndServe(":8080", router)
